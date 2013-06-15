1a3c5cb46c1f5a505bb2910b1ef4e1c53bcfa16f
karupanerura <karupa@cpan.org>
2013-06-16 01:17:10 +0900

    version up to 0.31

--- plenv.el
+++ plenv.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012 Kenta Sato
 
 ;; Author: Kenta Sato <karupa@cpan.org>
-;; Version: 0.22
+;; Version: 0.31
 ;; Keywords: Emacs, Perl
 
 ;; This program is free software; you can redistribute it and/or modify
